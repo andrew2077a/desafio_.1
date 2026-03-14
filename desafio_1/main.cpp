bfc73497309215bc39e56fd2fc6891f5686affee
andrew2077a <andres.garcia34@udea.edu.co>
2026-03-14 18:45:34 -0500

    matriz

--- desafio_1/main.cpp
+++ desafio_1/main.cpp
@@ -2,11 +2,30 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(){
+    int alto,ancho;//..
+    while (true){
+        cout<<"Ingrese el alto del tablero (minimo 8):";
+        cin>>alto;
+        if (alto>=8){
+            break;
+        }
+        else {
+            cout<<"Incorrecto, Ingrese otro numero"<<endl;
+        }
+
+    }
+    while (true){
+        cout<<"Ingrese el ancho del tablero (minimo 8 y multiplo de 8):";
+        cin>>ancho;
+        if(ancho>=8 && ancho%8==0){
+            break;
+        }
+        else{
+            cout<<"Incorrecto, Ingrese otro numero"<<endl;
+        }
+    }
     return 0;
+
+
 }
-
-
-//edicion numero 1.1
