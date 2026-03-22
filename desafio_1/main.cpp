ccb3c556ded78a47e8bcbfb831cc00750589c5f9
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-03-21 19:53:44 -0500

    Estructura completa del juego (Game Loop).cpp

--- desafio_1/main.cpp
+++ desafio_1/main.cpp
@@ -1,83 +1,116 @@
 #include <iostream>
+#include "funciones.h"
+
 using namespace std;
 
 
-unsigned char** crear_tablero(int ancho, int alto) {
+int main() {
+    bool jugando = true;
+    char tecla;
 
-    int bytes_por_fila = ancho / 8;
+    int alto_tablero, ancho_tablero;
 
-    unsigned char** tablero = new unsigned char*[alto];
-
-    for(int i = 0; i < alto; i++) {
-        tablero[i] = new unsigned char[bytes_por_fila];
-
-        for(int j = 0; j < bytes_por_fila; j++) {
-            tablero[i][j] = 0;
-        }
+    while (true){
+        cout<<"Ingrese el alto del tablero (minimo 8):";
+        cin>>alto_tablero;
+        if (alto_tablero>=8) break;
+        cout<<"Incorrecto, Ingrese otro numero\n";
     }
 
-    return tablero;
-}
-
-
-void destruir_tablero(unsigned char** tablero, int alto) {
-    for(int i = 0; i < alto; i++) {
-        delete[] tablero[i];
+    while (true){
+        cout<<"Ingrese el ancho del tablero (minimo 8 y multiplo de 8):";
+        cin>>ancho_tablero;
+        if(ancho_tablero>=8 && ancho_tablero%8==0) break;
+        cout<<"Incorrecto, Ingrese otro numero\n";
     }
-    delete[] tablero;
-}
 
 
-void impresion_matriz(int ancho, int alto, unsigned char** tablero) {
-    int bytes_por_fila = ancho / 8;
+    iniciar_aleatoriedad();
+    unsigned char** tablero = crear_tablero(ancho_tablero, alto_tablero);
 
-    for(int i = 0; i < alto; i++) {
-        for(int b = 0; b < bytes_por_fila; b++) {
+    //u_ancho y u_alto ---> variables temporales
+    unsigned char pieza_activa[4];
+    unsigned char u_ancho, u_alto;
+    int tipo_pieza;
 
-            for(int bit = 7; bit >= 0; bit--) {
-                if(tablero[i][b] & (1 << bit)) {
-                    cout << "#";
-                } else {
-                    cout << ".";
+
+    generar_pieza_aleatoria(pieza_activa, u_ancho, u_alto, tipo_pieza);
+
+    int p_ancho = u_ancho;
+    int p_alto = u_alto;
+
+
+    int pos_x = (ancho_tablero / 2) - 1;
+    int pos_y = -p_alto + 1;
+
+
+    while (jugando) {
+
+
+        imprimir_frame(ancho_tablero, alto_tablero, tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y);
+
+
+        cout << "Movimiento (a=izq, d=der, s=bajar, w=rotar, q=salir): ";
+        cin >> tecla;
+
+        switch (tecla) {
+
+        case 'a': case 'A':
+            pos_x--;
+            if (verificar_colision(ancho_tablero, alto_tablero, tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y)) {
+                pos_x++;
+            }
+            break;
+
+        case 'd': case 'D':
+            pos_x++;
+            if (verificar_colision(ancho_tablero, alto_tablero, tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y)) {
+                pos_x--;
+            }
+            break;
+
+        case 'w': case 'W':
+            intentar_rotar(pieza_activa, p_ancho, p_alto, pos_x, pos_y, ancho_tablero, alto_tablero, tablero);
+            break;
+
+        case 's': case 'S':
+            pos_y++;
+
+            if (verificar_colision(ancho_tablero, alto_tablero, tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y)) {
+
+                pos_y--;
+
+
+                fijar_pieza(tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y, ancho_tablero, alto_tablero);
+
+                eliminar_filas(ancho_tablero, alto_tablero, tablero);
+
+
+                generar_pieza_aleatoria(pieza_activa, u_ancho, u_alto, tipo_pieza);
+                p_ancho = u_ancho;
+                p_alto = u_alto;
+
+                pos_x = (ancho_tablero / 2) - 1;
+                pos_y = -p_alto + 1;
+
+                if (comprobar_game_over(ancho_tablero, alto_tablero, tablero, pieza_activa, p_ancho, p_alto, pos_x, pos_y)) {
+                    jugando = false;
                 }
             }
-        }
-        cout << endl;
-    }
-}
+            break;
 
-int main(){
-    int alto,ancho;
-    while (true){
-        cout<<"Ingrese el alto del tablero (minimo 8): ";
-        cin>>alto;
-        if (alto>=8 && alto<=30){
+        case 'q': case 'Q':
+            jugando = false;
+            break;
+
+        default:
             break;
         }
-        else {
-            if(alto>30){
-                cout<<"Esta altura sobrepasa los limites visibles del tablero. Ingrese otro numero"<<endl;
-            }
-            else{
-                cout<<"Incorrecto, Ingrese otro numero"<<endl;
-            }
-        }
-
-    }
-    while (true){
-        cout<<"Ingrese el ancho del tablero (minimo 8 y multiplo de 8): ";
-        cin>>ancho;
-        if(ancho>=8 && ancho%8==0){
-            break;
-        }
-        else{
-            cout<<"Incorrecto, Ingrese otro numero"<<endl;
-        }
     }
 
+    cout <<endl<<"GAME OVER. Has perdido malooooo."<<endl;
 
-    unsigned char** miMatriz = crear_tablero(ancho, alto);
 
-    impresion_matriz(ancho, alto, miMatriz);
-
+    destruir_tablero(tablero, alto_tablero);
+    return 0;
 }
